6a5f0ad5980051faac687eaea50acf97404b60c3
prajwal.bandak <prajwalbandak777@gmail.com>
2023-12-02 20:03:59 +0530

    added the top bar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import './index.css'
+import Topbar from './scenes/global/Topbar';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-dom';
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+
+    <CssBaseline/>
+   
+    <div className="app">
+      <div className='content'><Topbar/> </div>
+      <Routes>
+        <Route />
+      </Routes>
+   
       
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
 export default App;
+
+
